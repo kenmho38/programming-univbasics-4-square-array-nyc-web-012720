194b4ce18c79c2e96e8be6003e843d42286dd04d
Ken Ho <kenmho38@hotmail.com>
2020-01-13 20:03:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,8 @@
-square_array = [1,2,3,4,5]
-square_array.each do |i|
-   puts i
+def square_array(array)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
